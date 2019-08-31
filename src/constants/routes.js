0820e0aca3a518df03bd1fc3a9a04f921c473b43
raparicio6 <rodrigoaparicio6@gmail.com>
2019-08-31 08:54:02 -0300

    changing name to route

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -2,7 +2,7 @@ const Routes = {
   HOME: '/',
   LOGIN: '/login',
   RECOVER_PASSWORD: '/forgot_password',
-  REPO_CREATION: '/create_repository',
+  REPO_CREATION: '/repositories',
   TEAMS: '/teams',
   ADD_MEMBER_TO_TEAM: '/teams/add_member',
   ADD_MEMBER: '/add_member'
