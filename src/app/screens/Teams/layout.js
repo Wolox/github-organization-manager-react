def0b8a76cdc478ced83173b674ff2a6690e0c8d
Gabriel Porras <ghporras@gmail.com>
2019-08-31 04:27:20 -0500

    feat(teams): finished teams page with new design

--- src/app/screens/Teams/layout.js
+++ src/app/screens/Teams/layout.js
@@ -7,53 +7,55 @@ import InputLabelNew from "~components/InputLabelNew";
 import styles from './styles.module.scss';
 import {FIELDS} from "./constants";
 
-function TeamCreation({ handleSubmit }) {
+function TeamCreation({ handleSubmit, isError }) {
   return (
-    <>
-      <div className={`page-header ${styles.pageHeader}`} data-parallax="true" />
-      <div className="main main-raised">
-        <div className="profile-content">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-6 ml-auto mr-auto">
-                <div className={`card ${styles.card}`}>
-                  <form className="container" onSubmit={handleSubmit}>
-                    <div className="card-header text-center">
-                      <h4 className="card-title">Create a new team</h4>
-                    </div>
-                    <div className={`card-body ${styles.cardBody}`}>
-                      <div className="input-group">
-                        <div className="input-group-prepend">
-                          <span className="input-group-text">
-                            <i className="material-icons">people</i>
-                          </span>
-                        </div>
-                        <Field
-                          inputClassName="form-control"
-                          className="form-control"
-                          name={FIELDS.name}
-                          component={InputLabelNew}
-                          dataFor={FIELDS.name}
-                          inputId={FIELDS.name}
-                          inputType="text"
-                          label="Team name"
-                          placeholder="Team Name"
-                        />
-                      </div>
-                    </div>
-                    <div className={`footer text-center ${styles.footer}`}>
-                      <button type="submit" className="btn btn-primary btn-wd btn-lg">
-                        Create
-                      </button>
-                    </div>
-                  </form>
-                </div>
-              </div>
+    <div className={`card ${styles.card}`}>
+      <form className="container" onSubmit={handleSubmit}>
+        <div className="card-header text-center">
+          <h4 className="card-title">Create a new team</h4>
+        </div>
+        <div className={`card-body ${styles.cardBody}`}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <span className="input-group-text">
+                <i className="material-icons">people</i>
+              </span>
             </div>
+            <Field
+              inputClassName="form-control"
+              className="form-control"
+              name={FIELDS.name}
+              component={InputLabelNew}
+              dataFor={FIELDS.name}
+              inputId={FIELDS.name}
+              inputType="text"
+              label="Team name"
+              placeholder="Team Name"
+            />
           </div>
         </div>
-      </div>
-    </>
+        <div className={`footer text-center ${styles.footer}`}>
+          <button type="submit" className="btn btn-primary btn-wd btn-lg">
+            Create
+          </button>
+        </div>
+        {
+          isError &&
+            <div className="alert alert-danger">
+              <div className="container">
+                <div className="alert-icon">
+                  <i className="material-icons">error_outline</i>
+                </div>
+                <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true"><i className="material-icons">clear</i></span>
+                </button>
+                <b>Error Alert:</b> Damn man! You screwed up the server this time. You should find a good excuse for your
+                Boss...
+              </div>
+            </div>
+        }
+      </form>
+    </div>
   );
 }
 
