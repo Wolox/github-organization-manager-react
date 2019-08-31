965b0809a86d5d79499be99d3e3588081537754d
Gabriel Porras <ghporras@gmail.com>
2019-08-31 02:24:19 -0500

    feat(teams): adding template design to teams page

--- src/app/screens/Teams/layout.js
+++ src/app/screens/Teams/layout.js
@@ -4,28 +4,45 @@ import { Field, reduxForm } from 'redux-form';
 
 import InputLabel from '../../components/InputLabel';
 
+import styles from './styles.module.scss';
+
 function TeamCreation({ handleSubmit }) {
   return (
-    <form className="container" onSubmit={handleSubmit}>
-      <div className="form-group">
-        <span className="h1">Create a new team</span>
+    <>
+      <div className={`page-header header-filter ${styles.pageHeader}`} data-parallax="true" />
+      <div className="main main-raised">
+        <div className="profile-content">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6 ml-auto mr-auto">
+                <div className="profile">
+                  <form className="container" onSubmit={handleSubmit}>
+                    <div className="form-group">
+                      <span className="h1">Create a new team</span>
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="exampleInputEmail1">Team Name</label>
+                      <Field
+                        className="form-control"
+                        name="name"
+                        component={InputLabel}
+                        dataFor="name"
+                        inputId="name"
+                        inputType="text"
+                        label="Team name"
+                      />
+                    </div>
+                    <button type="submit" className="btn btn-primary">
+                      Create
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="form-group">
-        <label htmlFor="exampleInputEmail1">Team Name</label>
-        <Field
-          className="form-control"
-          name="name"
-          component={InputLabel}
-          dataFor="name"
-          inputId="name"
-          inputType="text"
-          label="Team name"
-        />
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Create
-      </button>
-    </form>
+     </>
   );
 }
 
