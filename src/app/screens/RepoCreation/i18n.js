f2696a12cb4923680ff0245b878bd003cbf15b3f
raparicio6 <rodrigoaparicio6@gmail.com>
2019-08-31 03:41:34 -0300

    correct names

--- src/app/screens/RepoCreation/i18n.js
+++ src/app/screens/RepoCreation/i18n.js
@@ -3,7 +3,7 @@ import i18next from 'i18next';
 i18next.addResources('es', 'RepoCreation', {
   createNewRepository: 'Crear nuevo repositorio',
   createNewRepositoryAdvise:
-    'Se requiere nombre y tecnología del proyecto. Enviar el formulario sin ellos no tendrá ningún efecto.',
+    'Se requiere nombre y tecnología/s del proyecto. Enviar el formulario sin ellos no tendrá ningún efecto.',
   projectName: 'Nombre del proyecto',
   private: 'Privado',
   create: 'Crear'
