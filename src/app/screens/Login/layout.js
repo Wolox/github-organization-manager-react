130eebd9836551672d002bb6f99c59343d6b6945
Elkin <elkin.bernal@wolox.co>
2019-09-18 11:08:44 -0500

    remove commented code in the login layout file

--- src/app/screens/Login/layout.js
+++ src/app/screens/Login/layout.js
@@ -18,48 +18,11 @@ class Login {
           <div className="card-header text-center">
             <h4 className="card-title">{t('Login:login')}</h4>
           </div>
-          {/* <div className="card-body">
-             <div className="input-group">
-              <div className="input-group-prepend">
-                <span className="input-group-text">
-                  <i className="material-icons">mail</i>
-                </span>
-              </div>
-              <InputLabelNew
-                label={t('Login:email')}
-                name={FIELDS.email}
-                inputId={FIELDS.email}
-                dataFor={FIELDS.email}
-                inputType="email"
-                inputClassName="form-control"
-                placeholder={t('Login:emailPlaceholder')}
-                handleChange={onEmailChange}
-              />
-            </div>
-            <div className="input-group">
-              <div className="input-group-prepend">
-                <span className="input-group-text">
-                  <i className="material-icons">lock_outline</i>
-                </span>
-              </div>
-              <InputLabelNew
-                label={t('Login:password')}
-                name={FIELDS.password}
-                inputId={FIELDS.password}
-                dataFor={FIELDS.password}
-                inputType="password"
-                inputClassName="form-control"
-                placeholder={t('Login:passwordPlaceholder')}
-                handleChange={onPasswordChange}
-              />
-            </div>
-          </div>*/}
           <div className={`footer text-center ${styles.footer}`}>
             <button type="submit" className="btn btn-primary btn-wd btn-lg">
               {t('Login:enter')}
             </button>
             <br />
-            {/* <a href={Routes.RECOVER_PASSWORD}>{t('Login:forgotPassword')}</a> */}
           </div>
         </form>
       </div>
