f425aefceea48c2f06c3a5764c6cc6d6c88da7bd
Elkin <elkin.bernal@wolox.co>
2019-09-19 08:20:08 -0500

    remove div tags unnecessaries

--- src/app/screens/Login/index.js
+++ src/app/screens/Login/index.js
@@ -6,12 +6,8 @@ import styles from './styles.module.scss';
 function LoginContainer() {
   return (
     <div className={`page-header header-filter ${styles.pageHeader}`}>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-4 col-md-6 ml-auto mr-auto">
-            <Login />
-          </div>
-        </div>
+      <div className="container col-lg-4 col-md-6 ml-auto mr-auto">
+        <Login />
       </div>
     </div>
   );
