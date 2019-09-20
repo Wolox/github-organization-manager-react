6d668d89e413f5494de2ce550934b37abde72973
Elkin <elkin.bernal@wolox.co>
2019-09-20 11:54:32 -0500

    fix warning

--- src/app/screens/Home/index.js
+++ src/app/screens/Home/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
+import Card from '../../components/Card';
+
 import styles from './styles.module.scss';
 
 import ROUTES from 'constants/routes';
-
-import Card from '~components/Card';
 
 function Home() {
   const teamsCards = [
