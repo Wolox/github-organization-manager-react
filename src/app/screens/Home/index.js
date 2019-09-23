5f3ebf03436779e9e27445f124865815214e0588
Elkin <elkin.bernal@wolox.co>
2019-09-23 06:46:38 -0500

    move Card component

--- src/app/screens/Home/index.js
+++ src/app/screens/Home/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import Card from '../../components/Card';
-
+import Card from './components/Card';
 import styles from './styles.module.scss';
 
 import ROUTES from 'constants/routes';
