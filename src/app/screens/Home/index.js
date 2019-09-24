f1b2473cd20fcfc4e21d11fcd1417f1f66fd49ca
Elkin <elkin.bernal@wolox.co>
2019-09-24 11:13:03 -0500

    move the constants above the function

--- src/app/screens/Home/index.js
+++ src/app/screens/Home/index.js
@@ -5,47 +5,47 @@ import styles from './styles.module.scss';
 
 import ROUTES from 'constants/routes';
 
-function Home() {
-  const teamsCards = [
-    {
-      info: 'Add a new member to a team',
-      descriptionAction: 'add member',
-      path: ROUTES.ADD_MEMBER_TO_TEAM,
-      icon: 'person_add'
-    },
-    {
-      info: 'Create a new team',
-      descriptionAction: 'create',
-      path: ROUTES.TEAMS,
-      icon: 'group_add'
-    }
-  ];
-  const repositoriesCards = [
-    {
-      info: 'Add a new member to a repo',
-      descriptionAction: 'add member',
-      path: ROUTES.ADD_MEMBER,
-      icon: 'add_circle'
-    },
-    {
-      info: 'Add a owner to a repo',
-      descriptionAction: 'add owner',
-      path: ROUTES.ADD_OWNER_TO_REPO,
-      icon: 'assignment_ind'
-    },
-    {
-      info: 'Create a new repo',
-      descriptionAction: 'create',
-      path: ROUTES.REPO_CREATION,
-      icon: 'book'
-    }
-  ];
+const teamsCards = [
+  {
+    info: 'Add a new member to a team',
+    descriptionAction: 'add member',
+    path: ROUTES.ADD_MEMBER_TO_TEAM,
+    icon: 'person_add'
+  },
+  {
+    info: 'Create a new team',
+    descriptionAction: 'create',
+    path: ROUTES.TEAMS,
+    icon: 'group_add'
+  }
+];
+const repositoriesCards = [
+  {
+    info: 'Add a new member to a repo',
+    descriptionAction: 'add member',
+    path: ROUTES.ADD_MEMBER,
+    icon: 'add_circle'
+  },
+  {
+    info: 'Add a owner to a repo',
+    descriptionAction: 'add owner',
+    path: ROUTES.ADD_OWNER_TO_REPO,
+    icon: 'assignment_ind'
+  },
+  {
+    info: 'Create a new repo',
+    descriptionAction: 'create',
+    path: ROUTES.REPO_CREATION,
+    icon: 'book'
+  }
+];
 
+function Home() {
   return (
     <div className={styles.container}>
-      <div className={`page-header ${styles.pageHeader}`} data-parallax="true">
+      <span className={`page-header ${styles.pageHeader}`} data-parallax="true">
         Wolox Repo Manager
-      </div>
+      </span>
       <div className={styles.section}>
         <h2 className={styles.sectionTitle}>Teams</h2>
         <div className={styles.sectionBody}>
