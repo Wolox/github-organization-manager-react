c00fef4a61b4fab14c7ae67aba531e153dff885b
Elkin <elkin.bernal@wolox.co>
2019-09-26 11:11:30 -0500

    refactor AuthService file

--- src/services/AuthServices.js
+++ src/services/AuthServices.js
@@ -1,35 +1,20 @@
 import api from '../config/api';
-import { actionCreators as authActions } from '../redux/Auth/actions';
+/* import { actionCreators as authActions } from '../redux/Auth/actions'; */
 
 import * as LocalStorageService from './LocalStorageService';
 
-export const setCurrentUser = currentUser => {
-  api.setHeader('Authorization', currentUser.sessionToken);
-  LocalStorageService.setSessionToken(currentUser.sessionToken);
+export const setCurrentUser = token => {
+  api.setHeader('Authorization', `Bearer ${token}`);
+  LocalStorageService.setSessionToken(token);
 };
 export const getCurrentUser = () => {
-  const currentSessionToken = LocalStorageService.getSessionToken();
+  const token = LocalStorageService.getSessionToken();
 
-  if (currentSessionToken) {
-    api.setHeader('Authorization', currentSessionToken);
-
+  if (token) {
+    api.setHeader('Authorization', `Bearer ${token}`);
     return true;
   }
 
   return false;
 };
 export const removeCurrentUser = () => LocalStorageService.removeSessionToken();
-
-export const authSetup = async dispatch => {
-  const currentUser = await getCurrentUser();
-
-  dispatch(authActions.init(currentUser));
-};
-
-export const login = () =>
-  // TODO: Implement call to authentication API here
-  new Promise(resolve => {
-    setTimeout(() => {
-      resolve({ ok: true, data: { sessionToken: 'token' } });
-    }, 750); // eslint-disable-line no-magic-numbers
-  });
