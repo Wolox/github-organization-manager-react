c0500a44bc8c2ca3eb6b0fdcc645c7b72d334f69
Elkin <elkin.bernal@wolox.co>
2019-09-26 10:06:14 -0500

    change Login Component from class to function

--- src/app/screens/Login/layout.js
+++ src/app/screens/Login/layout.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { t } from 'i18next';
 
 import { useAuth0 } from '../../../react-auth0-spa';
 
 import styles from './styles.module.scss';
 
-class Login extends Component {
-  handleOnSubmit = () => {
-    const { loginWithRedirect } = useAuth0();
-    loginWithRedirect({});
-  };
+function Login() {
+  const { loginWithRedirect } = useAuth0();
 
-  render() {
-    return (
-      <form onSubmit={this.handleOnSubmit} className={`card card-login ${styles.form}`}>
-        <div className="card-title card-header text-center">{t('Login:login')}</div>
-        <div className={`footer text-center ${styles.footer}`}>
-          <button type="submit" className="btn btn-primary btn-wd btn-lg">
-            {t('Login:enter')}
-          </button>
-        </div>
-      </form>
-    );
-  }
+  return (
+    <div className={`card card-login ${styles.form}`}>
+      <div className="card-title card-header text-center">{t('Login:login')}</div>
+      <div className={`footer text-center ${styles.footer}`}>
+        <button type="button" onClick={loginWithRedirect} className="btn btn-primary btn-wd btn-lg">
+          {t('Login:enter')}
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
