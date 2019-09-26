70a71fdf3f08969ac45f9b0dbe536465fd2340fd
Elkin <elkin.bernal@wolox.co>
2019-09-26 11:10:44 -0500

    refactor AuthenticatedRoute component

--- src/app/components/Routes/components/AuthenticatedRoute.js
+++ src/app/components/Routes/components/AuthenticatedRoute.js
@@ -1,84 +1,37 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
 
-import Routes from '../../../../constants/routes';
+import ROUTES from 'constants/routes';
 
-const DEFAULT_PUBLIC_ROUTE = Routes.LOGIN;
+function AuthenticatedRoute({ isPrivate = false, isAuthenticated, component: Comp, ...props }) {
+  if (!isAuthenticated && isPrivate) {
+    return <Redirect to={ROUTES.LOGIN} />;
+  }
+  if (isAuthenticated && !isPrivate) {
+    return <Redirect to={ROUTES.HOME} />;
+  }
 
-function AuthenticatedRoute({
-  /*
-   * TODO Add this if you need it
-   * device,
-   */
-  isPublicRoute,
-  isPrivateRoute,
-  // initialized,
-  currentUser,
-  component: Comp,
-  ...props
-}) {
   return (
     <Route
       {...props}
       // eslint-disable-next-line react/jsx-no-bind
-      render={routeProps => {
-        /*
-         * TODO Add this if you need it
-         * if (device.isMobile && !device.adviceSubmitted) {
-         *   return <AppDownloader />;
-         * }
-         */
-        if (currentUser) {
-          if (isPublicRoute) {
-            /*
-             * TODO Add this if you need it
-             * if (currentUser && isPublicRoute) {
-             * do not allow logged users to access public routes. redirect to app
-             */
-            // return (
-            //   <Redirect
-            //     to={{
-            //       pathname: DEFAULT_PRIVATE_ROUTE,
-            //       state: { from: props.location }
-            //     }}
-            //   />
-            // );
-          }
-        } else if (isPrivateRoute) {
-          // Do not allow unlogged users to access app. redirect to signin
-          return (
-            <Redirect
-              to={{
-                pathname: DEFAULT_PUBLIC_ROUTE,
-                state: { from: props.location }
-              }}
-            />
-          );
-        }
-
-        return <Comp {...routeProps} />;
-      }}
+      render={routeProps => <Comp {...routeProps} />}
     />
   );
 }
 
-AuthenticatedRoute.defaultProps = {
-  /*
-   * TODO Add this if you need it
-   * isPublicRoute: true,
-   */
-  currentUser: false
-};
-
 AuthenticatedRoute.propTypes = {
   component: PropTypes.elementType,
-  currentUser: PropTypes.bool,
-  isPrivateRoute: PropTypes.bool,
-  isPublicRoute: PropTypes.bool,
+  isAuthenticated: PropTypes.bool,
+  isPrivate: PropTypes.bool,
   location: PropTypes.objectOf(PropTypes.any)
 };
 
-export default withRouter(connect()(AuthenticatedRoute));
+const mapStateToProps = state => ({
+  isAuthenticated: !!state.auth.currentUser
+});
+
+export default withRouter(connect(mapStateToProps)(AuthenticatedRoute));
