d4af741c256fefd2bd7d795b79d5684c978314d2
Elkin <elkin.bernal@wolox.co>
2019-09-26 11:17:25 -0500

    change App Component from class to function

--- src/app/index.js
+++ src/app/index.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 
 import { apiSetup } from '../config/api';
 import store from '../redux/store';
 
+import SimpleSpinner from './components/SimpleSpinner';
 import Routes from './components/Routes';
+
+import { setCurrentUser, getCurrentUser } from 'services/AuthServices';
+import { authInit, logout } from 'redux/Auth/actions';
+import { useAuth0 } from 'react-auth0-spa';
+
 import '../scss/application.scss';
 
-class App extends Component {
-  componentDidMount() {
+function App() {
+  const { getTokenSilently, loading, user, isAuthenticated } = useAuth0();
+  const getToken = async () => {
+    const hasToken = await getCurrentUser();
+    if (!loading && !hasToken) {
+      const token = await getTokenSilently();
+      setCurrentUser(token);
+    }
+  };
+  useEffect(() => {
     apiSetup(store.dispatch);
-  }
+    if (!loading && !isAuthenticated) {
+      store.dispatch(logout());
+    } else {
+      getToken();
+      store.dispatch(authInit(user));
+    }
+  }, [user, isAuthenticated]);
 
-  render() {
-    return (
-      <Provider store={store}>
-        <Routes />
-      </Provider>
-    );
-  }
+  return <Provider store={store}>{loading ? <SimpleSpinner /> : <Routes />}</Provider>;
 }
-
-App.defaultProps = {
-  loading: false
-};
 
 export default App;
