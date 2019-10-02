419488980c82c8fd33e3d079ef0beee53a35c413
Elkin <elkin.bernal@wolox.co>
2019-10-02 08:31:25 -0500

    use Route component in Login Component

--- src/app/components/Routes/index.js
+++ src/app/components/Routes/index.js
@@ -1,6 +1,7 @@
 import React, { lazy } from 'react';
 import { ConnectedRouter } from 'connected-react-router';
 import { Switch } from 'react-router-dom';
+import { Route } from 'react-router';
 
 import Routes from '~constants/routes';
 import { history } from '~redux/store';
@@ -22,13 +23,13 @@ function AppRoutes() {
     <ConnectedRouter history={history}>
       <Suspense>
         <Switch>
-          <AuthenticatedRoute exact path={Routes.LOGIN} component={Login} />
-          <AuthenticatedRoute isPrivate path={Routes.HOME} exact component={Home} />
-          <AuthenticatedRoute isPrivate path={Routes.REPO_CREATION} exact component={RepoCreation} />
-          <AuthenticatedRoute isPrivate path={Routes.TEAMS} exact component={Teams} />
-          <AuthenticatedRoute isPrivate path={Routes.ADD_MEMBER_TO_TEAM} exact component={AddMemberToTeam} />
-          <AuthenticatedRoute isPrivate path={Routes.ADD_MEMBER} exact component={AddMember} />
-          <AuthenticatedRoute isPrivate path={Routes.ADD_OWNER_TO_REPO} exact component={AddOwnerToRepo} />
+          <Route path={Routes.LOGIN} component={Login} />
+          <AuthenticatedRoute path={Routes.HOME} exact component={Home} />
+          <AuthenticatedRoute path={Routes.REPO_CREATION} exact component={RepoCreation} />
+          <AuthenticatedRoute path={Routes.TEAMS} exact component={Teams} />
+          <AuthenticatedRoute path={Routes.ADD_MEMBER_TO_TEAM} exact component={AddMemberToTeam} />
+          <AuthenticatedRoute path={Routes.ADD_MEMBER} exact component={AddMember} />
+          <AuthenticatedRoute path={Routes.ADD_OWNER_TO_REPO} exact component={AddOwnerToRepo} />
         </Switch>
       </Suspense>
     </ConnectedRouter>
