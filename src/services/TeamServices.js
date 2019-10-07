01171b07cae3e74aca7ae8f6ed1eaf5483482e5b
Elkin <elkin.bernal@wolox.co>
2019-10-07 10:12:25 -0500

    remove mockapi

--- src/services/TeamServices.js
+++ src/services/TeamServices.js
@@ -1,25 +1,10 @@
-import { create } from 'apisauce';
-
 import api from '../config/api';
 
-/*
 export const createTeam = values => api.post('/teams', values);
-export const addMembersToTeam = values => {
+
+export const addMembersToTeam = values =>
   api.post(`/teams/${values.team}/members`, {
     usernames: values.usernames.split(',')
   });
-}; */
 
 export const getTeams = () => api.get('/teams');
-
-export const createTeam = values =>
-  create({
-    baseURL: 'https://jsonplaceholder.typicode.com',
-    timeout: 55000
-  }).post('/posts', values);
-
-export const addMembersToTeam = values =>
-  create({
-    baseURL: 'https://jsonplaceholder.typicode.com',
-    timeout: 55000
-  }).post('/posts', values);
