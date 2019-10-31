7ea6d588513a0a4939e143b1d5eae10ef6de2266
Elkin <elkin.bernal@wolox.co>
2019-10-31 09:16:58 -0500

    list teams

--- src/app/screens/Teams/index.js
+++ src/app/screens/Teams/index.js
@@ -4,31 +4,66 @@ import PropTypes from 'prop-types';
 
 import teamActions from '~redux/Team/actions';
 import Header from '~components/Header';
+import SimpleSpinner from '~components/SimpleSpinner';
+
+import styles from './styles.module.scss';
 
 class TeamsContainer extends Component {
-  handleSubmit = () => {
+  componentDidMount() {
     this.props.getTeams();
-  };
+  }
 
   render() {
+    const { data, loading } = this.props;
     return (
       <>
         <Header />
-        <div>Soy una lista de equipo</div>
+        <div className={`${styles.content} card card-body`}>
+          <div className={styles.header}>
+            <div className={`${styles.cell} title`}>Nombre</div>
+            <div className={`${styles.cell} title`}>Descripción</div>
+            <div className={`${styles.cell} title`}>Privacidad</div>
+          </div>
+          <div className={styles.body}>
+            {loading ? (
+              <SimpleSpinner classNameContainer={styles.spinner} />
+            ) : (
+              data.map(team => (
+                <div className={styles.row} key={team.id}>
+                  <div className={styles.cell}>{team.name}</div>
+                  <div className={styles.cell}>{team.description}</div>
+                  <div className={styles.cell}>{team.privacy}</div>
+                </div>
+              ))
+            )}
+          </div>
+        </div>
       </>
     );
   }
 }
 
-TeamsContainer.propTypes = {
-  getTeams: PropTypes.func
+TeamsContainer.defaultProps = {
+  data: [],
+  loading: false
 };
+
+TeamsContainer.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.any),
+  getTeams: PropTypes.func,
+  loading: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  data: state.team.data,
+  loading: state.team.dataLoading
+});
 
 const mapDispatchToProps = dispatch => ({
   getTeams: () => dispatch(teamActions.getTeams())
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(TeamsContainer);
